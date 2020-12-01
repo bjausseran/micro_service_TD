117b18cba0b06b9883b9f02351e668407ea7c121
bjausseran <benoit.jausseran@ynov.com>
2020-12-01 15:50:27 +0100

    Comment ok

---
+++ spring.coment.service/src/main/java/com/example/demo/coment/ComentRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.coment;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComentRepository extends CrudRepository<Coment, Integer>{
+
+}
