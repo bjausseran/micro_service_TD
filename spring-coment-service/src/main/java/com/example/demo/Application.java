7507ffede8d3fbfed9ac7e612de37e0ff7e7e69a
bjausseran <benoit.jausseran@ynov.com>
2020-12-07 13:39:15 +0100

    service registry ok

--- spring-coment-service/src/main/java/com/example/demo/Application.java
+++ spring-coment-service/src/main/java/com/example/demo/Application.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import java.util.List;
+
+@EnableFeignClients
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication(proxyBeanMethods = false)
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+
+}
 }
